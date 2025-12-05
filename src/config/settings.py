20f8a7aa7875563e12bda02f9777304f3e558298
Ilnur <ilnur.altezza@yandex.ru>
2025-12-05 12:30:29 +0300

    Подключил базу данных PostgreSQL

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,8 +75,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "hotel_booking"),
+        "USER": os.environ.get("POSTGRES_USER", "hotel_booking"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "hotel_booking"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5433"),
     }
 }
 
